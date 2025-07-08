e5c7311f450a7adb329d97b35fdc8f553be695b9
kyle6193 <kyle.transue@gmail.com>
2025-07-08 12:01:42 -0400

    updated educational comments

--- Balloon Popper/balloon_manager.gd
+++ Balloon Popper/balloon_manager.gd
@@ -4,8 +4,8 @@ extends Node3D
 var score : int = 0
 @export var score_text : Label # @export makes the variable editable in the Inspector Dock
 
-func increase_score(amount):
-	score += amount
+func increase_score(amount): 
+	score += amount 
 	score_text.text = str("Score: ", score) # adds "Score:" before the integer for the score. 
 
 # As a whole this script creates the score keeping and displaying to the screen
--- Loops/loops.gd
+++ Loops/loops.gd
@@ -4,33 +4,31 @@ extends Node2D
 # @export makes the variable editable in the Inspector Dock
 @export var spawn_count : int = 200
 @export var spawn_interval : float = 0.025
-var star_scene = preload("res://Loops/star.tscn") #this adds a file to the game at launch. In this case the star scene
+var star_scene = preload("res://Loops/star.tscn") #this adds a file to the game at launch. In this case, the star scene
 var time_passed : float = 0.0
 var stars_spawned : int = 0
 
 func _process(delta: float) -> void:
 	if stars_spawned < spawn_count:
-		time_passed += delta # Add time since last frame
-		while time_passed >= spawn_interval and stars_spawned < spawn_count:
+		time_passed += delta # Add time since last frame to the time_passed variable
+		while time_passed >= spawn_interval and stars_spawned < spawn_count: # Checks if time_passed is greater than the spawn interval due to something like a lag spike
+			# If so, spawns a star and corrects the count, accounting for the spike
 			spawn_star() # Spawn a new star
 			stars_spawned += 1 # Increment the count
 			time_passed -= spawn_interval # Subtract the interval to keep accurate timing
+			# This ensures that even if the game lags, stars will still spawn at the correct rate
 			
 
 func spawn_star():
-	# Instantiates and configures a new star
-	var star = star_scene.instantiate() # Create a new star instance
-	add_child(star) # Add the star to the scene tree
-	
+	var star = star_scene.instantiate() # instantiate adds the star to memory based on the preload from above
+	add_child(star) # This line literally adds the star to the scene tree, making it visible in the game
 	# Randomize star position within a defined range
 	star.position.x = randi_range(-280, 280)
 	star.position.y = randi_range(-150, 150)
-	
 	# Randomize star size
 	var star_size = randf_range(0.5, 1.0)
 	star.scale.x = star_size
 	star.scale.y = star_size
-	
 	# Linear interpolation to simulate redshift of stars as they travel away from camera
 	var min_scale = 0.5 #smallest a star can be
 	var max_scale = 1.0 #largest a star can be
